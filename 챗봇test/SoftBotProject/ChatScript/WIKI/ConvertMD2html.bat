e81efd0bbfe91ff79b950def60b4db296dbe5903
Taeyoung Kim <fkam12@naver.com>
2017-07-22 20:11:52 +0900

    170722 20:10 태영
    chatscript 자료
    이것만으로 실행안됨

---
+++ 챗봇test/SoftBotProject/ChatScript/WIKI/ConvertMD2html.bat
@@ -0,0 +1,47 @@
+pandoc ../README.html -s -o ../README.html
+pandoc ChatScript-Advanced-User-Manual.html -s -o ../HTMLDOCUMENTATION/ChatScript-Advanced-User-Manual.html
+pandoc ChatScript-Basic-User-Manual.html -s -o ../HTMLDOCUMENTATION/ChatScript-Basic-User-Manual.html
+pandoc ChatScript-Debugging-Manual.html -s -o ../HTMLDOCUMENTATION/ChatScript-Debugging-Manual.html
+pandoc ChatScript-Fact-Manual.html -s -o ../HTMLDOCUMENTATION/ChatScript-Fact-Manual.html
+pandoc ChatScript-Finalizing-A-Bot.html -s -o ../HTMLDOCUMENTATION/ChatScript-Finalizing-A-Bot.html
+pandoc ChatScript-Json.html -s -o ../HTMLDOCUMENTATION/ChatScript-Json.html
+pandoc ChatScript-Debugger.html -s -o ../HTMLDOCUMENTATION/ChatScript-Debugger.html
+pandoc ChatScript-Memorization.html -s -o ../HTMLDOCUMENTATION/ChatScript-Memorization.html
+pandoc ChatScript-Overview-of-Input-to-Output.html -s -o ../HTMLDOCUMENTATION/ChatScript-Overview-of-Input-to-Output.html
+pandoc ChatScript-Pattern-Redux.html -s -o ../HTMLDOCUMENTATION/ChatScript-Pattern-Redux.html
+pandoc ChatScript-Spelling-Marking.html -s -o ../HTMLDOCUMENTATION/ChatScript-Spelling-Marking.html
+pandoc ChatScript-System-Functions-Manual.html -s -o ../HTMLDOCUMENTATION/ChatScript-System-Functions-Manual.html
+pandoc ChatScript-System-Variables-and-Engine-defined-Concepts.html -s -o ../HTMLDOCUMENTATION/ChatScript-System-Variables-and-Engine-defined-Concepts.html
+pandoc ChatScript-Command-Line-Parameters.html -s -o ../HTMLDOCUMENTATION/ChatScript-Command-Line-Parameters.html
+pandoc Installing-and-Updating-ChatScript.html -s -o ../HTMLDOCUMENTATION/Installing-and-Updating-ChatScript.html
+pandoc ChatScript-Multiple-Bots.html -s -o ../HTMLDOCUMENTATION/ChatScript-Multiple-Bots.html
+
+pandoc README.html -s -o ../HTMLDOCUMENTATION/README.html
+
+pandoc CLIENTS-AND-SERVERS/ChatScript-Amazon-Server.html -s -o ../HTMLDOCUMENTATION/CLIENTS-AND-SERVERS/ChatScript-Amazon-Server.html
+pandoc CLIENTS-AND-SERVERS/ChatScript-ClientServer-Manual.html -s -o ../HTMLDOCUMENTATION/CLIENTS-AND-SERVERS/ChatScript-ClientServer-Manual.html
+pandoc CLIENTS-AND-SERVERS/ChatScript-External-Communications.html -s -o ../HTMLDOCUMENTATION/CLIENTS-AND-SERVERS/ChatScript-External-Communications.html
+
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Analytics-Manual.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Analytics-Manual.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Control-Scripts.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Control-Scripts.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Document-Reader.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Document-Reader.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Exotica-Examples.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Exotica-Examples.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Javascript.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Javascript.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-MongoDB.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-MongoDB.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Planning.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Planning.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-PosParser.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-PosParser.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-PostgreSQL.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-PostgreSQL.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Engine.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Engine.html
+pandoc ESOTERIC-CHATSCRIPT/ChatScript-Foreign-Languages.html -s -o ../HTMLDOCUMENTATION/ESOTERIC-CHATSCRIPT/ChatScript-Foreign-Languages.html
+
+pandoc OVERVIEWS-AND-TUTORIALS/ChatScript-Tutorial.html -s -o ../HTMLDOCUMENTATION/OVERVIEWS-AND-TUTORIALS/ChatScript-Tutorial.html
+pandoc OVERVIEWS-AND-TUTORIALS/What-is-ChatScript.html -s -o ../HTMLDOCUMENTATION/OVERVIEWS-AND-TUTORIALS/What-is-ChatScript.html
+
+pandoc PAPERS/Paper-ARBOR-MakingItReal.html -s -o ../HTMLDOCUMENTATION/PAPERS/Paper-ARBOR-MakingItReal.html
+pandoc PAPERS/Paper-Google-Talk.html -s -o ../HTMLDOCUMENTATION/PAPERS/Paper-Google-Talk.html
+
+pandoc PREDEFINED-BOTS/Bot-German.html -s -o ../HTMLDOCUMENTATION/PREDEFINED-BOTS/Bot-German.html
+pandoc PREDEFINED-BOTS/Bot-Harry.html -s -o ../HTMLDOCUMENTATION/PREDEFINED-BOTS/Bot-Harry.html
+pandoc PREDEFINED-BOTS/Bot-NLTK.html -s -o ../HTMLDOCUMENTATION/PREDEFINED-BOTS/Bot-NLTK.html
+pandoc PREDEFINED-BOTS/Bot-Postgres.html -s -o ../HTMLDOCUMENTATION/PREDEFINED-BOTS/Bot-Postgres.html
+pandoc PREDEFINED-BOTS/Bot-Stockpile.html -s -o ../HTMLDOCUMENTATION/PREDEFINED-BOTS/Bot-Stockpile.html
---
+++ 챗봇test/SoftBotProject/ChatScript/WIKI/once.bat
@@ -0,0 +1,3 @@
+
+pandoc ChatScript-Multiple-Bots.html -s -o ../PDFDOCUMENTATION/ChatScript-Multiple-Bots.pdf
+pause
